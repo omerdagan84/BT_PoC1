999baa0cdd51ecc56df0593275992418ce0b21d2
Omer Dagan <omerdagan84@gmail.com>
2015-03-17 20:55:37 +0200

    state machine fixes

--- RN_52_control/RN_52_control.ino
+++ RN_52_control/RN_52_control.ino
@@ -18,8 +18,8 @@ char key_input;
 char board[500];
 char sink1[15];
 char sink2[15];
-int index = 0;
-int mFlag = 0;
+//int index = 0;
+//int mFlag = 0;
 int loopControl = 0;
 void reset_source(){
   Serial.println("TRACE: reset_source called...");
@@ -39,10 +39,12 @@ void reset_sink(){
   delay(2000);
 }
 void establish_source_connection(){
-    Serial.println("TRACE: establish_source_connection called...");
   /*function polls connection status - exit only upon connection to the source*/
     char temp;
-    mFlag = 0;
+    int mFlag = 0;
+    int index = 0;                            //reset index
+
+    Serial.println("TRACE: establish_source_connection called...");
     while(mFlag == 0){
     index = 0;                            //reset index
     digitalWrite(YEL_LED, LOW);           //turn off LED
@@ -52,7 +54,6 @@ void establish_source_connection(){
     {
       temp = mserial_1.read();
       board[index++] =  temp; //read input into buffer
-      Serial.write(temp);
     }
 
     if (index == 0 ) //checks connection to Source module
@@ -64,7 +65,7 @@ void establish_source_connection(){
     delay(500);
     if(index == 6) {       //if the correct two byte response was recieved
       Serial.print("Source Status");
-      Serial.println(board[0]+board[1]+board[2]+board[3]);
+      Serial.print(board[0]);Serial.print(board[1]);Serial.print(board[2]);Serial.println(board[3]);
       if (board[3] == '3') {               //4th HEX hold connection status - 3 connected
         index = 0;                        //reset index of buffer
         mFlag = 1;                        //connection made - move on
@@ -74,27 +75,32 @@ void establish_source_connection(){
   }
 }
 int verify_source_connection(){
-      Serial.println("TRACE: verify_source_connection called...");
   /*function queries source module
       returns:
       -1 - Error on communicating with module
       0  - Module responding, but no connection is established
       1  - Module responding and connected to source*/
     char temp;
-    mFlag = 0;
-    index = 0;                            //reset index
-
-    mserial_1.println("q");                 //send query command to device
-    delay(500);
-    while(mserial_1.available() > 0)      //while device still has data to input
-    {
-      temp = mserial_1.read();
-      board[index++] =  temp; //read input into buffer
-      Serial.write(temp);
+    int mFlag = 0;
+    int index = 0;                            //reset index
+    int loop_control = 5;
+    mserial_1.begin(9600);//reinitialize the UART connection
+    delay(1000);
+    Serial.println("TRACE: verify_source_connection called...");
+    while(loop_control-- > 0){
+         mserial_1.println("q");                 //send query command to device
+        delay(1000);
+        while(mserial_1.available() > 0)      //while device still has data to input
+        {
+          temp = mserial_1.read();
+          board[index++] =  temp; //read input into buffer
+        }
+        if(index>0) loop_control =0;
     }
-
+    
     if (index == 0 ) {//checks connection to Source module
           digitalWrite(RED_LED, HIGH);
+          Serial.println("TRACE: verify_source_connection returning -1 due to index 0 on read ");
           return -1;
     }
     else
@@ -103,8 +109,8 @@ int verify_source_connection(){
 
     delay(500);
     if(index == 6) {       //if the correct two byte response was recieved
-      Serial.print("Source Status");
-      Serial.println(board[0]+board[1]+board[2]+board[3]);
+      Serial.print("Source Status: ");
+        Serial.print(board[0]);Serial.print(board[1]);Serial.print(board[2]);Serial.println(board[3]);
       if (board[3] == '3') {               //4th HEX hold connection status - 3 connected
         index = 0;                        //reset index of buffer
         mFlag = 1;                        //connection made - move on
@@ -118,7 +124,7 @@ int verify_source_connection(){
     }
 }
 void setup_sink_module_2(){
-        Serial.println("TRACE: setup_sink_module_2 called...");
+   Serial.println("TRACE: setup_sink_module_2 called...");
    digitalWrite(M2_EN, HIGH);              //turn on second module
    delay(5000);
    mserial_2.begin(9600);
@@ -129,57 +135,61 @@ void setup_sink_module_2(){
    mserial_2.begin(9600);
    delay(1000);
 }
-void establish_sink_1_connection(){
+void establish_sink_2_connection(){
     char temp;
-    Serial.println("TRACE: establish_sink_1_connection called...");
-    mFlag = 0;
+    int mFlag = 0;
+    int index = 0;                            //reset index
+    int mFound1 = 0;
+    int mFound2 = 0;
+    Serial.println("TRACE: establish_sink_2_connection called...");
     while (mFlag == 0){
-    index = 0;                            //reset index
-    digitalWrite(BLU_LED, LOW);           //turn off LED
-    mserial_2.println("f,9");                 //send query command to device
-    delay(10000);
-    while(mserial_2.available() > 0)      //while device still has data to input
-    {
-      temp = mserial_2.read();  //read input into buffer
-      board[index++] = temp;  //read input into buffer
-      Serial.print(temp);
-    }
-
-    digitalWrite(BLU_LED, HIGH);          //turn on LED
-    delay(500);
-    
-    for (int i = 0; i < index; i++) {
-      if ((board[i] == 'F')  && (board[i+6]==':')) {
-        Serial.println("found available connection");
-        if (mFlag==0) {
-          Serial.println("first ");
-          Serial.println(i);
-          sink1[0]='c';sink1[1]=',';
-          for (int j = 0; j <= 12;j++) {
-            sink1[j+2] = board[i+j+7];
+          index = 0;                            //reset index
+          digitalWrite(BLU_LED, LOW);           //turn off LED
+          mserial_2.println("f,9");                 //send query command to device
+          delay(10000);
+          while(mserial_2.available() > 0)      //while device still has data to input
+          {
+            temp = mserial_2.read();  //read input into buffer
+            board[index++] = temp;  //read input into buffer
           }
-          sink1[15]='\0';
-          Serial.println("Sending connection commmand on second discoverd device");
-          Serial.println(sink1);
-          mserial_2.println(sink1);  
-          delay(50);
-          mFlag=1;
-        } else {
-           Serial.println("second ");
-           Serial.println(i);
-           sink2[0]='c';sink2[1]=',';
-          for (int j = 0; j <= 12;j++) {
-            sink2[j+2] = board[i+j+7];
+      
+          digitalWrite(BLU_LED, HIGH);          //turn on LED
+          delay(500);
+          
+          for (int i = 0; i < index; i++) {
+            if ((board[i] == 'F')  && (board[i+6]==':')) {
+              Serial.println("found available connection");
+              if (mFlag==0) {
+                mFound1 = 1;
+                Serial.println("first ");
+                Serial.println(i);
+                sink1[0]='c';sink1[1]=',';
+                for (int j = 0; j <= 12;j++) {
+                  sink1[j+2] = board[i+j+7];
+                }
+                sink1[15]='\0';
+                Serial.println("Sending connection commmand on second discoverd device");
+                Serial.println(sink1);
+                mserial_2.println(sink1);  
+                delay(50);
+                mFlag=1;
+              } else {
+                 mFound2 = 1;
+                 Serial.println("second ");
+                 Serial.println(i);
+                 sink2[0]='c';sink2[1]=',';
+                for (int j = 0; j <= 12;j++) {
+                  sink2[j+2] = board[i+j+7];
+                }
+                sink2[15]='\0';
+                  Serial.println("Sending connection commmand on first discoverd device");
+                  Serial.println(sink2);
+                  mserial_2.println(sink2);  
+                  delay(500);
+              }
+            }
+           
           }
-          sink2[15]='\0';
-            Serial.println("Sending connection commmand on first discoverd device");
-            Serial.println(sink2);
-            mserial_2.println(sink2);  
-            delay(500);
-        }
-      }
-     
-    }
   }
   mFlag=0;
   while(mFlag ==0){
@@ -193,38 +203,43 @@ void establish_sink_1_connection(){
     }
     if(index == 6) {       //if the correct two byte response was recieved
       Serial.print("Sink Status");
-      Serial.println(board[0]+board[1]+board[2]+board[3]);
+      Serial.print(board[0]);Serial.print(board[1]);Serial.print(board[2]);Serial.println(board[3]);
       if (board[3] == '8' || board[3] == '9') {               //4th HEX hold connection status - 3 connected
         index = 0;                        //reset index of buffer
         mFlag = 1;                        //connection made - move on
       } else {
-        mserial_2.println(sink1);  
+        if (mFound1) mserial_2.println(sink1);  
         delay(50);
-        mserial_2.println(sink2);
+        if (mFound2) mserial_2.println(sink2);
         delay(10000); 
       }
     }
     index = 0;
   }
 }
-int verify_sink_1_connection(){
+int verify_sink_2_connection(){
     /*function queries sink module
       returns:
       -1 - Error on communicating with module
       0  - Module responding, but no connection is established
       1  - Module responding and connected to sink*/
-    char temp;
-        Serial.println("TRACE: verify_sink_1_connection called...");
-    mFlag = 0;
-    index = 0;                            //reset index
-
-    mserial_2.println("q");                 //send query command to device
-    delay(500);
-    while(mserial_1.available() > 0)      //while device still has data to input
-    {
-      temp = mserial_1.read();
-      board[index++] =  temp; //read input into buffer
-      Serial.write(temp);
+    char temp;   
+    int mFlag = 0;
+    int index = 0;                            //reset index
+    int loop_control = 5;
+ 
+    mserial_2.begin(9600);//reinitialize the UART connection
+    delay(1000);   
+    Serial.println("TRACE: verify_sink_1_connection called...");
+    while(loop_control-- > 0){
+         mserial_2.println("q");                 //send query command to device
+        delay(1000);
+        while(mserial_2.available() > 0)      //while device still has data to input
+        {
+          temp = mserial_2.read();
+          board[index++] =  temp; //read input into buffer
+        }
+        if(index>0) loop_control =0;
     }
 
     if (index == 0 ) {//checks connection to Source module
@@ -237,8 +252,8 @@ int verify_sink_1_connection(){
 
     delay(500);
     if(index == 6) {       //if the correct two byte response was recieved
-      Serial.print("Sink Status");
-      Serial.println(board[0]+board[1]+board[2]+board[3]);
+      Serial.print("Sink Status: ");
+      Serial.print(board[0]);Serial.print(board[1]);Serial.print(board[2]);Serial.println(board[3]);
       if (board[3] == '8' || board[3] == '9') {               //4th HEX hold connection status - 3 connected
         index = 0;                        //reset index of buffer
         mFlag = 1;                        //connection made - move on
@@ -250,6 +265,12 @@ int verify_sink_1_connection(){
        return 0;
       }
     }
+}
+void shutdown_sink_2_module(){
+  Serial.println("TRACE: vshutdown_sink_2_module called...");
+  digitalWrite(M2_EN, LOW);
+  mserial_2.begin(9600);
+  mserial_2.println("r,1");//turn of sink module
 }
 typedef enum State{
   error,
@@ -274,17 +295,17 @@ void setup()
 
   //set EN pins to low
   digitalWrite(M1_EN, HIGH);
-  digitalWrite(M2_EN, LOW);
+  
   
   //turn on serial interfaces to first module and forward messages to DEBUG
   Serial.begin(115200); 
- Serial.println("modules");
+ Serial.println("TRACE: setup preparing modules");
   mserial_1.begin(9600);
-  mserial_2.begin(9600);
+  
   
   //GREen led ON
   digitalWrite(GRE_LED, HIGH);
-  mserial_2.println("r,1");//turn of sink module
+  shutdown_sink_2_module();
   reset_source();
   mState = boot;
 }
@@ -294,17 +315,13 @@ void loop() // run over and over
   
   switch(mState){
     case boot:
-        index = 0;
-        mFlag = 0;
         establish_source_connection();
         Serial.println("Connected to BT source");
         mState = source_connected;
         break;
     case source_connected:
-        index = 0;
-        mFlag = 0;
         setup_sink_module_2();
-        establish_sink_1_connection();
+        establish_sink_2_connection();
         Serial.println("Connected to BT sink");
         mState = sink_connected;
         break;
@@ -316,11 +333,12 @@ void loop() // run over and over
                 break;
           case -1:
                 Serial.println("Connection to BT source module LOST"); 
+                shutdown_sink_2_module();
                 mState = error;
                 break;
           case 1:
                 Serial.println("Connection to BT source OK"); 
-                switch(verify_sink_1_connection()){
+                switch(verify_sink_2_connection()){
                     case 0:
                           Serial.println("Connection to BT sink LOST"); 
                           mState = source_connected;
